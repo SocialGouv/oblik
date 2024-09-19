4a2a1291bdad1e9fa94e657d23f43c03ce76557d
devthejo <devthejo@protonmail.com>
2024-09-19 22:49:30 +0200

    fix: wip

--- tests/e2e.sh
+++ tests/e2e.sh
@@ -2,6 +2,6 @@
 set -eo errexit
 export KUBECONFIG="${HOME}/.kube/config"
 
-# $(dirname $0)/setup-cluster.sh
+$(dirname $0)/setup-cluster.sh
 $(dirname $0)/deploy-oblik.sh
 $(dirname $0)/test-oblik.sh $@
