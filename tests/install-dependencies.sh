9802eae8a644ddb450f655773ec8f2ef2e143820
devthejo <devthejo@protonmail.com>
2024-09-19 23:27:30 +0200

    fix: wip

--- tests/install-dependencies.sh
+++ tests/install-dependencies.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -eo errexit
 
-export KUBECONFIG="${HOME}/.kube/config"
-
-$(dirname $0)/kind-with-registry.sh
-
 helm repo add kyverno https://kyverno.github.io/kyverno/
 helm repo update
 helm upgrade --install kyverno kyverno/kyverno --namespace kyverno --create-namespace --version=3.2.5
